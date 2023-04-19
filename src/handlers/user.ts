b3460ac5562e04f97d49a77e40729c78bd15e641
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-04-19 21:52:22 +0100

    Feat: Create  User Handler
    I created a user handler that accepts request from the routes and
    interacts with the user model to perform the computation. I also
    implemented jwts for tokenization.

---
+++ src/handlers/user.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import { User, UserStore } from '../models/user';
+import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express';
+
+dotenv.config();
+
+const store = new UserStore();
+const {TOKEN_SECRET} = process.env
+
+export class UserHandler {
+  async create(_req: Request, res: Response) {
+    const { username, password } = _req.body;
+    const user: User = {
+      username,
+      password,
+    };
+
+    try {
+        const newUser = await store.create(user);
+        let token = jwt.sign({user: newUser}, String(TOKEN_SECRET));
+        res.status(201).send(token);
+    } catch (error) {
+        res.status(400);
+        res.json(String(error) + user);
+    }
+  }
+}
