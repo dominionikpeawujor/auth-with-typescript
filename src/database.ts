614d2a455c2cea37bd40832701c1cc4c4022f53e
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-04-19 14:13:38 +0100

    Feat: Add Migrations for Postgres database
    I used db-migrate to create a migration for my databases so I do not
    have to manually set up the database in docker. I can automatically
    create tables, etc with the db-migrate up command.
    
    I also made edits to the database.ts file in order to accommodate
    testing. I would not want tests to be run on the live database.
    
    I also installed some packages for the database configuration.

--- src/database.ts
+++ src/database.ts
@@ -3,14 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST } =
+const { POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, ENV, POSTGRES_TEST_DB } =
   process.env;
 
+
+  var db: string = '';
+  ENV === 'test'
+    ? (db = POSTGRES_TEST_DB as string)
+    : ENV === 'dev'
+    ? (db = POSTGRES_DB as string)
+    : '';
+  
 const client = new Pool({
     host: POSTGRES_HOST,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
-    database: POSTGRES_DB   
+    database: db   
 })
 
 export default client;
