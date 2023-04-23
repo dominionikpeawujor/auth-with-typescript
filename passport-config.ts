b37631ce3fea248033949d7eaa0c61a378d20eca
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-04-23 22:37:55 +0100

    Feat: Add Passport Session to the to allow login
    I added the passport session functionality so that users can stay logged
    into the app. Majorly for authentication purposes.

--- passport-config.ts
+++ passport-config.ts
@@ -1,25 +1,27 @@
-// import passportLocal from 'passport-local';
-// const LocalStrategy = passportLocal.Strategy;
-// import bcrypt from 'bcrypt';
+import passportLocal from 'passport-local';
+const LocalStrategy = passportLocal.Strategy;
+import bcrypt from 'bcrypt';
 
-// function initialize(passport: any) {
-//     const authenticateUsers = async (email:string, password: string, done: any) => {
-//         const user = getUserByEmail(email);
-//         if(user == null){
-//             return done(null, false, ({message: "No use found with that email"}));
-//         }
-//         try {
-//             if(await bcrypt.compare(password, user.password)){
-//                 return done(null, user);
-//             }
-//         } catch (error) {
-//             console.log(error);
-//             return done(error);
-//         }
-//     }
-//     passport.use(new LocalStrategy({usernameField: 'email'}));
-//     passport.serializeUser((user,done)=> {})
-//     passport.deserializeUser((id,done)=> {})
-// }
-
-// module.exports = initialize;
+export function initialize(passport: any,getUserByEmail: any, getUserById: any) {
+    const authenticateUsers = async (email:string, password: string, done: any) => {
+        const user = getUserByEmail(email);
+        if(user == null){
+            return done(null, false, ({message: "No use found with that email"}));
+        }
+        try {
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user);
+            } else {
+                return done(null, false, {message: "Password incorrect"})
+            }
+        } catch (error) {
+            console.log(error);
+            return done(error);
+        }
+    }
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUsers));
+    passport.serializeUser((user: { id: any; },done: (arg0: null, arg1: any) => any)=> done(null, user.id))
+    passport.deserializeUser((id: any, done: (arg0: null, arg1: any) => any)=> {
+        return done(null, getUserById(id))
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,47 @@
+import dotenv from 'dotenv';
 import express, { Application, Request, Response, urlencoded } from 'express';
 import routes from './routes';
-// import initializePassport ;
+import {initialize} from '../passport-config';
+import passport from 'passport';
+import flash from 'express-flash';
+import session from 'express-session';
+
+if(process.env.NODE_ENV !== 'production')
+{
+  dotenv.config();
+}
+
+
+initialize(passport, ((email: any) => users.find((user: { email: any; }) => user.email === email)), ((id: any) => users.find((user: { id: any; }) => user.id === id)))
+
+const users: any = [];
 
 const app: Application = express();
 const PORT: Number = 3000;
 
 app.use(urlencoded({extended: false}));
 
+app.use(flash());
+app.use(session({
+  secret: process.env.SECRET_KEY!,
+  resave: false, //We won't receive session variable if nothing changed.
+  saveUninitialized: false,
+}))
+app.use(passport.initialize());
+app.use(passport.session);
+
+
 app.use('/api', routes);
+
+app.post('/login', passport.authenticate("local", {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}))
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Server Connected.');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Connected to port ${PORT}`);
